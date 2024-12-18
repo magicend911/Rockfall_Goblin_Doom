ba9efb6fd6c6e72f22ba7d343baa05ee2f94b5b0
Anton Ferzov <magicend911@outlook.com>
2024-12-18 23:00:31 +0700

    add sound

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,16 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] private Button _start; // Приватная кнопка, видимая в инспекторе
+    [SerializeField] private Button _start;
 
-    private void Awake()
+    private void OnEnable()
     {
-        // Привязываем метод к событию нажатия на кнопку
         _start.onClick.AddListener(LoadScene1);
+    }
+
+    private void OnDisable()
+    {
+        _start.onClick.RemoveListener(LoadScene1);
     }
 
     private void LoadScene1()
     {
-        SceneManager.LoadScene(1); // Загружаем сцену с индексом 1
+        SceneManager.LoadScene(1);
     }
 }
