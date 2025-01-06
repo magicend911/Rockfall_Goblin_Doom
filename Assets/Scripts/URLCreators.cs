a311f2aa1621973b09744690a6f73d32216022fe
Anton Ferzov <magicend911@outlook.com>
2025-01-06 21:52:15 +0700

    added URL for autors

---
+++ Assets/Scripts/URLCreators.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class URLCreators : MonoBehaviour
+{
+    public void URL2D()
+    {
+        Application.OpenURL("https://www.artstation.com/davydova");
+    }
+
+    public void URLProg()
+    {
+        Application.OpenURL("https://github.com/magicend911");
+    }
+
+    public void URL3D()
+    {
+        Application.OpenURL("https://t.me/Marssyr");
+    }
+
+    public void URL3DE()
+    {
+        Application.OpenURL("https://www.artstation.com/myafka");
+    }
+
+    public void URLSound()
+    {
+        Application.OpenURL("https://t.me/someonesome7");
+    }
+
+    public void URLGame()
+    {
+        Application.OpenURL("https://t.me/A_Ganin");
+    }
+}
